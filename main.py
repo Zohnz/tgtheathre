2ce6ce975a6f20cbe7c61a7cf88fcbeb45f01bd2
Ariadne <116460998+Zohnz@users.noreply.github.com>
2023-07-08 14:17:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import config as cfg
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+from background import keep_alive
 
 logging.basicConfig(level=logging.INFO)
 from db import Database
@@ -15,15 +16,29 @@ db = Database('database.db')
 with open('quotes.txt', 'r') as file:
     quotes = file.readlines()
 
+
 @dp.message_handler(commands=['quote'])
 async def send_random_quote(message: types.Message):
     random_quote = random.choice(quotes)
     await message.reply(random_quote)
 
+
+with open('books.txt', 'r') as file:
+    books = file.readlines()
+
+
+@dp.message_handler(commands=['book'])
+async def send_random_book(message: types.Message):
+    random_book = random.choice(books)
+    await message.reply(random_book)
+
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await bot.send_message(message.from_user.id, "Привет {0.first_name} введи свой вопрос".format(message.from_user),
-                           reply_markup=nav.mainMenu)
+    await bot.send_message(message.from_user.id,
+                           "Привет! Данный бот предлагает погрузиться в театральную атмосферу и заглянуть в закулисье. С помощью команды /quote Вы можете получить любую цитату о театре, а также задать любой вопрос боту напрямую.".format(
+                               message.from_user),
+                           reply_markup=nav.btnMain)
 
 
 @dp.message_handler()
@@ -34,20 +49,14 @@ async def mess(message: types.Message):
 
 @dp.message_handler()
 async def bot_message(message: types.Message):
-    if message.text == 'Число удачи':
-        await bot.send_message(message.from_user.id, 'Ваше удачное число на день: ' + str(random.randint(1, 10)))
+    if message.text == 'Спектакль онлайн}':
+        await bot.send_message(message.from_user.id,
+                               "Для того, чтобы посмотреть знаменитые спектакли не выходя из дома рекомендуем Вам сервис https://www.mos.ru/city/projects/kulturaonline/theatre/ ! Теперь не нужно ездить в другой город и тратить деньги. Приятного просмотра!".format(
+                                   message.from_user),
+                               reply_markup=nav.btnOnline)
 
-    # elif message.text == '':
-    #     await bot.send_message(message.from_user.id, '', reply_markup + nav.mainMenu)
-    #
-    # elif message.text == '':
-    #     await bot.send_message(message.from_user.id, '', reply_markup + nav.otherMenu)
-    #
-    # elif message.text == '':
-    #     await bot.send_message(message.from_user.id, '')
-    #
-    # elif message.text == '':
-    #     await bot.send_message(message.from_user.id, '')
 
+keep_alive()
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
